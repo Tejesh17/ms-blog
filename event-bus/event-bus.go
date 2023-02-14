4c1df128991eb3b3d8be4c6b7f4f3f08398b86e0
Tejesh <tejesh.paidi@omnibiz.com>
2023-02-15 00:17:43 +0530

    small change

--- event-bus/event-bus.go
+++ event-bus/event-bus.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func events(c *gin.Context) {
-
-	body, _ := ioutil.ReadAll(c.Request.Body)
-
-	http.Post("http://localhost:8080/eventbus", "application/json", bytes.NewReader(body))
-	http.Post("http://localhost:8081/eventbus", "application/json", bytes.NewReader(body))
-	http.Post("http://localhost:8082/eventbus", "application/json", bytes.NewReader(body))
-
+	http.Post("http://localhost:8080/eventbus", "application/json", c.Request.Body)
+	http.Post("http://localhost:8081/eventbus", "application/json", c.Request.Body)
+	http.Post("http://localhost:8082/eventbus", "application/json", c.Request.Body)
 }
